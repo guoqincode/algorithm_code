a1b197e99f29d3b3c47fc263bdae264fc1282bda
qguoxdu <55274489+guoqincode@users.noreply.github.com>
2021-06-11 14:49:46 +0800

    Create 2020.9-2.cpp

--- THU/2020.9-2.cpp
+++ THU/2020.9-2.cpp
@@ -7,7 +7,7 @@
 //具体地，有n台机器，[1...n],m个读写操作
 
 //R t x1 x2 ... xw 表示在t时间访问了x1...xw
-//如果不足w台数据相同，则从1号开始访问未访问过的，直到有w台相同
+//如果不足w台数据相同，则从1号开始访问未访问过的，直到有w台 相同
 //找到最新数据后，把本次访问的没有最新成功提交数据的机器更新为最新成功提交的数据
 //-->读操作也可能产生更新操作
 //输出：最新成功提交的数据版本t和访问的机器数目v,立刻输出
@@ -21,5 +21,40 @@
 
 //保证对于同一时间，可能会有多个读请求，但是只会有一个写请求
 
+//n<=100 m<=3*1e4 n/2<w<=n d<=1000
+
 #include<bits/stdc++.h>
 using namespace std;
+int n,m,w,d;
+const int maxn=101,maxm=3*1e4+5,maxw=3*1e4+5,maxd=1000;
+char op;
+int R_W[maxw],W_W[maxn];
+
+struct Node{
+    int cur_data;   //目前的数据是哪个时间点留下的
+    
+}node[maxn];
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin>>n>>m>>w>>d;
+    while(m--){
+        cin>>op;
+        int t;
+        if(op=='R'){
+            cin>>t;
+            for(int i=1;i<=w;i++) cin>>R_W[i];
+
+            //read fuction
+
+        }else if(op=='W'){
+            cin>>t;
+            int k;
+            cin>>k;
+            for(int i=1;i<=k;i++) cin>>W_W[i];
+
+            //write fuction
+
+        }
+    }
+}
